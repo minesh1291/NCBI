049d3a9d19d1f809d04e6ee0d50a4c1275026d7f
Minesh Jethva <minesh.1291@gmail.com>
2016-09-05 12:15:24 +0300

    Update dl_seq.py

--- dl_seq.py
+++ dl_seq.py
@@ -1,10 +1,15 @@
 from Bio import Entrez
 from Bio import SeqIO
+
 Entrez.email = "my_name@my_website.com"
 id_list = set(open('pids_test.csv', 'rU'))
+id_list = list(id_list)
+
 handle = Entrez.efetch(db="protein", rettype="fasta", retmode="text", id=id_list)	
+
 out_handle = open("saved.fasta", "w")
 for line in handle:
   out_handle.write(line)
+
 out_handle.close()
 handle.close()
